dd168326b8f76abf109a1436a58ffaf67b1281cb
AeroIIite <72459704+AeroIIite@users.noreply.github.com>
2025-04-22 18:59:21 -0500

    Create weather_api.py

---
+++ weather_api.py
@@ -0,0 +1,37 @@
+import requests
+
+
+def get_weather_data(city_name):
+    api_key = "117592278e2f56a31b2d3c65659e57c0"  # Replace with your actual API key
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data
+    else:
+        print(f"Error fetching weather data: {response.status_code}")
+        return None
+
+
+def get_coordinates(city_name):
+    api_key = "117592278e2f56a31b2d3c65659e57c0"  # Replace with your actual API key
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        lat = data['coord']['lat']  # Latitude
+        lon = data['coord']['lon']  # Longitude
+        print(f"Latitude: {lat}, Longitude: {lon}")
+        return lat, lon
+    else:
+        print(f"Error fetching data: {response.status_code}")
+        return None, None
+
+
+def get_forecast(lat, lon):
+    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,alerts&units=imperial&appid=117592278e2f56a31b2d3c65659e57c0"
+    response = requests.get(url)
+    return response.json()
