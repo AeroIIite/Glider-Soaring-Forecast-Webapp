d21b85a4b69c87017f43af68c196ad3bb661e092
AeroIIite <72459704+AeroIIite@users.noreply.github.com>
2025-04-22 22:11:07 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -48,8 +48,8 @@ def format_weather_data(data):
     return report
 
 # Streamlit app
-st.title("⛅ Soaring Forecast Pro")
-st.subtitle("Current date and time")
+st.title("⛅ GlideWx: Current Conditions")
+
 location = st.text_input("Enter your glider location (e.g., Memphis):")
 
 if location:
